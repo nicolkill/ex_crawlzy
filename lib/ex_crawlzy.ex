fb033f0b64e4a918ecfc9192d12e69d5bbe5aeed
Nicol Acosta <nicolacosta8@gmail.com>
2023-07-31 14:28:57 -0600

    project base

---
+++ lib/ex_crawlzy.ex
@@ -0,0 +1,18 @@
+defmodule ExCrawlzy do
+  @moduledoc """
+  Documentation for `ExCrawlzy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCrawlzy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_crawlzy_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCrawlzyTest do
+  use ExUnit.Case
+  doctest ExCrawlzy
+
+  test "greets the world" do
+    assert ExCrawlzy.hello() == :world
+  end
+end
